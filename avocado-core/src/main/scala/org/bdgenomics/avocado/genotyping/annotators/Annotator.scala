ebb19b54bc8a6348096d4af829d238fafc7ce5f5
andrewmchen <andrewmchen@berkeley.edu>
2015-04-16 16:16:27 -0700

    Added functionality to create a VariantCallingAnnotation

---
+++ avocado-core/src/main/scala/org/bdgenomics/avocado/genotyping/annotators/Annotator.scala
@@ -0,0 +1,113 @@
+/**
+ * Licensed to Big Data Genomics (BDG) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The BDG licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bdgenomics.avocado.genotyping.annotators
+
+import org.bdgenomics.avocado.algorithms.math.LogBinomial
+import org.bdgenomics.avocado.algorithms.math.LogToPhred.log2phred
+import org.bdgenomics.avocado.models.AlleleObservation
+import org.bdgenomics.formats.avro.{ Variant, VariantCallingAnnotations }
+
+object VariantCallingAnnotator {
+  def apply(variant: Variant,
+            observations: Iterable[AlleleObservation],
+            variantQuality: Double,
+            likelihoods: Array[Double]): VariantCallingAnnotator = {
+    new VariantCallingAnnotator(variant, observations, variantQuality, likelihoods)
+  }
+}
+
+class VariantCallingAnnotator(variant: Variant, observations: Iterable[AlleleObservation], variantQuality: Double, likelihoods: Array[Double]) {
+
+  val alt = variant.getAlternateAllele
+  val ref = variant.getReferenceAllele
+  val altObservations = observations.filter(_.allele == alt)
+  val refObservations = observations.filter(_.allele == ref)
+  val numAlt = altObservations.size
+  val numRef = refObservations.size
+
+  val annotationBuilder = VariantCallingAnnotations.newBuilder()
+
+  def build(): VariantCallingAnnotations = {
+    annotationBuilder.setVariantQualityByDepth(variantQualityByDepth())
+      .setReadDepth(readDepth())
+      .setMqRankSum(mapQRankSum())
+      .setFisherStrandBiasPValue(fisherStrandBiasValue())
+      .setRmsMapQ(rmsMapQ())
+      .build()
+  }
+
+  def variantQualityByDepth(): Float = {
+    (variantQuality / numAlt).toFloat
+  }
+
+  def readDepth(): Integer = {
+    numAlt + numRef
+  }
+
+  // Calculate RankSum as according to https://www.broadinstitute.org/gatk/guide/article?id=473
+  def mapQRankSum(): Float = {
+    val sortedObservations: Array[AlleleObservation] = (altObservations ++ refObservations).toArray.sortBy(_.mapq)
+    //Make (observation, rank) tuples
+    val rankedObservations: Array[(AlleleObservation, Float)] = sortedObservations.map((x: AlleleObservation) => (x, 0f))
+
+    for (idx <- 0 until sortedObservations.length) {
+      val curMapQ = sortedObservations(idx).mapq
+
+      // Scan above and under for same mapq
+      var minIdx = idx
+      while (minIdx > 0 && sortedObservations(minIdx - 1).mapq == curMapQ) {
+        minIdx = minIdx - 1
+      }
+      var maxIdx = idx
+      while (maxIdx < sortedObservations.size - 1 && sortedObservations(maxIdx + 1).mapq == curMapQ) {
+        maxIdx = maxIdx + 1
+      }
+
+      val rank = (minIdx + maxIdx) / 2f + 1
+      rankedObservations(idx) = rankedObservations(idx).copy(_2 = rank)
+    }
+
+    val rankRef = rankedObservations.filter(_._1.allele == ref).map(_._2).reduce(_ + _)
+    val rankAlt = rankedObservations.filter(_._1.allele == alt).map(_._2).reduce(_ + _)
+
+    val uRef = rankRef - numRef * (numRef + 1) / 2f
+    val uAlt = rankAlt - numAlt * (numAlt + 1) / 2f
+
+    val mu = (numRef * numAlt) / 2f
+    val std = math.sqrt((numRef * numAlt * (numRef + numAlt + 1)) / 12f)
+    ((uAlt - mu) / std).toFloat
+  }
+
+  def fisherStrandBiasValue(): Float = {
+    val negativeAlts = altObservations.filter(_.onNegativeStrand).size
+    val positiveAlts = altObservations.filter(!_.onNegativeStrand).size
+    val negativeRefs = refObservations.filter(_.onNegativeStrand).size
+    val positiveRefs = refObservations.filter(!_.onNegativeStrand).size
+    val n = positiveAlts + positiveRefs + negativeAlts + negativeRefs
+    val logP = LogBinomial.logBinomial(positiveRefs + positiveAlts, positiveRefs) +
+      LogBinomial.logBinomial(negativeRefs + negativeAlts, negativeRefs) -
+      LogBinomial.logBinomial(n, positiveRefs + negativeRefs)
+    log2phred(logP).toFloat
+  }
+
+  def rmsMapQ(): Float = {
+    val mapqObservations = observations.filter(!_.mapq.isEmpty)
+    val meanSquareMapQ = (mapqObservations.map(x => math.pow(x.mapq.get.toDouble, 2)).reduce(_ + _)) / mapqObservations.size.toFloat
+    math.sqrt(meanSquareMapQ).toFloat
+  }
+}
---
+++ avocado-core/src/test/scala/org/bdgenomics/avocado/genotyping/annotators/VariantCallingAnnotatorSuite.scala
@@ -0,0 +1,84 @@
+package org.bdgenomics.avocado.genotyping.annotators
+
+import org.bdgenomics.avocado.algorithms.math.MathTestUtils
+import org.bdgenomics.avocado.genotyping.BiallelicGenotyper
+import org.scalatest.FunSuite
+import org.bdgenomics.formats.avro.{ Variant, VariantCallingAnnotations }
+import org.bdgenomics.adam.models.{SequenceRecord, SequenceDictionary, ReferencePosition}
+import org.bdgenomics.avocado.algorithms.math.LogToPhred.log2phred
+import org.bdgenomics.avocado.models.AlleleObservation
+
+import scala.math.{ log, pow, sqrt }
+
+class VariantCallingAnnotatorSuite extends FunSuite {
+  val ba = new BiallelicGenotyper(SequenceDictionary(SequenceRecord("ctg", 1000L)),
+    Map(("ctg", 1000L)))
+  val ref = "A"
+  val alt = "G"
+  val variant = Variant.newBuilder()
+                       .setReferenceAllele(ref)
+                       .setAlternateAllele(alt)
+                       .build()
+
+  val variantQuality = 35d
+
+  def createAllele(base: String, mapQ: Int, strand: Boolean): AlleleObservation = {
+    AlleleObservation(ReferencePosition("ctg", 0L),
+      1,
+      base,
+      30,
+      Some(mapQ),
+      strand,
+      "mySample",
+      1L)
+  }
+
+  test("annotate variant quality by depth") {
+    val observed = Iterable(createAllele(ref, 20, true),
+                            createAllele(ref, 30, true),
+                            createAllele(alt, 40, true))
+
+    val likelihoods = ba.scoreGenotypeLikelihoods(ref, alt, observed)._2
+    val annotator = VariantCallingAnnotator(variant, observed, variantQuality, likelihoods)
+    MathTestUtils.assertAlmostEqual(annotator.variantQualityByDepth(), variantQuality)
+  }
+
+  test("annotate read depth") {
+    val observed = Iterable(createAllele(ref, 20, true),
+                            createAllele(ref, 30, true),
+                            createAllele(alt, 40, true))
+    val likelihoods = ba.scoreGenotypeLikelihoods(ref, alt, observed)._2
+    val annotator = VariantCallingAnnotator(variant, observed, variantQuality, likelihoods)
+    assert(annotator.readDepth() == 3)
+  }
+
+  test("annotate map quality rank sum") {
+    val refBaseQ = List(20, 25, 26, 30, 32, 40, 47, 50, 53, 60)
+    val altBaseQ = List(0, 7, 10, 17, 20, 21, 30, 34, 40, 45)
+    val observed = refBaseQ.map(createAllele(ref, _, true)) ++ altBaseQ.map(createAllele(alt, _, false))
+    val likelihoods = ba.scoreGenotypeLikelihoods(ref, alt, observed)._2
+    val annotator = VariantCallingAnnotator(variant, observed, variantQuality, likelihoods)
+    MathTestUtils.assertAlmostEqual(annotator.mapQRankSum(), -2.154f, 1e-3)
+  }
+
+  test("annotate fisher strand bias value") {
+    val observed = Iterable(createAllele(ref, 20, true),
+                            createAllele(ref, 30, true),
+                            createAllele(alt, 40, false),
+                            createAllele(alt, 40, false))
+    val likelihoods = ba.scoreGenotypeLikelihoods(ref, alt, observed)._2
+    val annotator = VariantCallingAnnotator(variant, observed, variantQuality, likelihoods)
+    MathTestUtils.assertAlmostEqual(annotator.fisherStrandBiasValue(), log2phred(log(1/6f)))
+  }
+
+  test("annotate rms map q") {
+    val observed = Iterable(createAllele(ref, 20, true),
+                            createAllele(ref, 30, true),
+                            createAllele(alt, 40, false),
+                            createAllele(alt, 40, false))
+    val likelihoods = ba.scoreGenotypeLikelihoods(ref, alt, observed)._2
+    val annotator = VariantCallingAnnotator(variant, observed, variantQuality, likelihoods)
+    val rmsMapQ = sqrt((pow(20d, 2d) + pow(30d, 2d) + pow(40d, 2d) + pow(40d, 2d))/4d)
+    MathTestUtils.assertAlmostEqual(annotator.rmsMapQ(), rmsMapQ)
+  }
+}
