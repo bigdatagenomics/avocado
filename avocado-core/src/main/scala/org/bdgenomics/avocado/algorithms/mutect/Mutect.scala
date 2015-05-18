aac84f9ee006b85b863b4a6613bebe32b6229787
John St John <johnthesaintjohn@gmail.com>
2015-05-18 16:48:21 -0700

    Adding mutect tests and TODO comments to main code

--- avocado-core/src/main/scala/org/bdgenomics/avocado/algorithms/mutect/Mutect.scala
+++ avocado-core/src/main/scala/org/bdgenomics/avocado/algorithms/mutect/Mutect.scala
@@ -31,6 +31,8 @@ import org.bdgenomics.formats.avro.{ Contig, Genotype, Variant }
 class Mutect(val threshold: Double = 6.3, val f: Option[Double] = None) {
 
   val model = MutectLogOdds
+  // TODO: do we want to do somatic classification here?
+  // val somaticClassifier = MutectSomaticLogOdds
   val alleles: Set[String] = Set("A", "T", "G", "C")
 
   // TODO make sure we only consider the tumor AlleleObservations for this calculation
@@ -61,16 +63,27 @@ class Mutect(val threshold: Double = 6.3, val f: Option[Double] = None) {
              ref: String,
              obs: Seq[AlleleObservation]): Option[(Variant, Seq[Genotype])] = {
 
+    //TODO do we want to split up normal/tumor here, or earlier in code?
+    val tumors = obs.filter(a => a.sample == "tumor")
+    //val normals = obs.filter(a => a.sample == "normal")
+
     val rankedAlts: Seq[(Double, String)] =
       (alleles - ref).map { alt =>
         (model.logOdds(ref, alt, obs, f), alt)
       }.toSeq.sorted.reverse
 
-    val (logOdds, alt) = rankedAlts(0)
+    //TODO here we should check/flag if there are multiple variants that pass the threshold, this is a
+    // downstream filtering criteria we have easy access to right here.
+    val passingOddsAlts = rankedAlts.filter(oa => oa._1 >= threshold)
 
-    if (logOdds >= threshold) {
+    // TODO do we want to output any kind of info for sites where multiple passing variants are found
+    if (passingOddsAlts.size == 1) {
+      // TODO classify somatic status here? or as a downstream step?
+
+      val alt = passingOddsAlts(0)._2
       Option(constructVariant(pos, ref, alt, obs))
     } else {
+      // either there are 0 passing variants, or there are > 1 passing variants
       None
     }
   }
--- avocado-core/src/test/scala/org/bdgenomics/avocado/algorithms/mutect/MutectSuite.scala
+++ avocado-core/src/test/scala/org/bdgenomics/avocado/algorithms/mutect/MutectSuite.scala
@@ -1,10 +1,52 @@
 package org.bdgenomics.avocado.algorithms.mutect
 
+import org.bdgenomics.adam.models.ReferencePosition
+import org.bdgenomics.avocado.models.AlleleObservation
 import org.scalatest.FunSuite
 
 /**
  * Created by john on 5/17/15.
  */
 class MutectSuite extends FunSuite {
+  val pos = ReferencePosition("ctg", 101l)
+  val mt = new Mutect()
+  //Demo data
+  val all_c = for (read_id <- 1 to 10) yield {
+    AlleleObservation(pos = ReferencePosition("ctg", 0L),
+      length = 1,
+      allele = "C",
+      phred = 30 + (read_id - 1), // 30 to 39
+      mapq = Some(30),
+      onNegativeStrand = true,
+      sample = "tumor",
+      readId = 1L)
+  }
+
+  val some_muts = for (read_id <- 1 to 10) yield {
+    AlleleObservation(pos = ReferencePosition("ctg", 0L),
+      length = 1,
+      allele = if (read_id <= 3) "A" else "C", //Mutants = A x 3 w/ scores (30,31,32)
+      phred = 30 + (read_id - 1), // 30 to 39
+      mapq = Some(30),
+      onNegativeStrand = true,
+      sample = "tumor",
+      readId = 1L)
+  }
+
+  test("Sites with no mutations should not have any variants returned") {
+    val result = mt.detect(pos, "C", all_c)
+    assert(result.isEmpty, "Result should be empty")
+  }
+
+  test("Sites with mutations should return a variant") {
+    val result = mt.detect(pos, "C", some_muts)
+    assert(result.isDefined, "Result should not be empty")
+    val (variant, genotype) = result.get
+    assert(variant.getAlternateAllele === "A", "Alternate allele should be a")
+    assert(variant.getReferenceAllele === "C", "Reference allele should be c")
+    assert(variant.getContig.getContigName === "ctg", "Contig should be 'ctg'")
+    assert(variant.getStart === 101l, "Variant should be at position 101")
+    assert(variant.getEnd === 102l, "Variant should be at position 101")
+  }
 
 }
