3a4025cd754bdee9464ade8ed7a34d3cc86a42de
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2016-09-23 15:32:57 -0700

    Cleaned up avocado run scripts and packaging.

--- avocado-cli/src/main/scala/org/bdgenomics/avocado/AvocadoMain.scala
+++ avocado-cli/src/main/scala/org/bdgenomics/avocado/AvocadoMain.scala
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.bdgenomics.adam.cli
+package org.bdgenomics.avocado.cli
 
 import org.bdgenomics.utils.cli._
 import org.bdgenomics.utils.misc.Logging
