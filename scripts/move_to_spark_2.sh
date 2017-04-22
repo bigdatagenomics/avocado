17c0e3e4416ccb26d1b0d77d830bce0ccc07b4bd
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2017-04-22 15:19:30 -0700

    [AVOCADO-232] Make move_to_* scripts rename artifacts.
    Resolves #232. Added avocado pattern to `move_to_spark2.sh` and added Scala 2.10
    suffixes to artifact names in POMs.

--- scripts/move_to_spark_2.sh
+++ scripts/move_to_spark_2.sh
@@ -17,7 +17,9 @@ substitution_cmd="s/_$svp/-spark2_$svp/g"
 find . -name "pom.xml" -exec sed \
     -e "/utils-/ s/_2\.1/-spark2_2.1/g" \
     -e "/adam-/ s/_2\.1/-spark2_2.1/g" \
+    -e "/avocado-/ s/_2\.1/-spark2_2.1/g" \
     -e "/utils-/ $substitution_cmd" \
     -e "/adam-/ $substitution_cmd" \
+    -e "/avocado-/ $substitution_cmd" \
     -e "/spark.version/ s/1.6.3/2.1.0/g" \
     -i.spark2.bak '{}' \;
