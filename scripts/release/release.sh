6b6ada57cd4388845e5ad30b75fbee7ae32e64d0
Frank Austin Nothaft <frank.nothaft@databricks.com>
2018-01-08 10:42:23 -0800

    Add updated release scripts.

---
+++ scripts/release/release.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+# do we have enough arguments?
+if [ $# < 3 ]; then
+    echo "Usage:"
+    echo
+    echo "./release.sh <release version> <development version>"
+    exit 1
+fi
+
+# pick arguments
+release=$1
+devel=$2
+
+# get current branch
+branch=$(git status -bs | awk '{ print $2 }' | awk -F'.' '{ print $1 }' | head -n 1)
+
+# manually edit and commit changelog changes
+# see https://github.com/bigdatagenomics/adam/issues/936
+#./scripts/changelog.sh $1 | tee CHANGES.md
+#git commit -a -m "Modifying changelog."
+
+commit=$(git log --pretty=format:"%H" | head -n 1)
+echo "releasing from ${commit} on branch ${branch}"
+
+git push origin ${branch}
+
+# do scala 2.10 release
+git checkout -b maint_2.10-${release} ${branch}
+mvn --batch-mode \
+  -P distribution \
+  -Dresume=false \
+  -Dtag=avocado-parent_2.10-${release} \
+  -DreleaseVersion=${release} \
+  -DdevelopmentVersion=${devel} \
+  -DbranchName=avocado_2.10-${release} \
+  release:clean \
+  release:prepare \
+  release:perform \
+  -Darguments="-DskipTests"
+
+if [ $? != 0 ]; then
+  echo "Releasing Scala 2.10 version failed."
+  exit 1
+fi
+
+# do scala 2.11 release
+git checkout -b maint_2.11-${release} ${branch}
+./scripts/move_to_scala_2.11.sh
+git commit -a -m "Modifying pom.xml files for Scala 2.11 release."
+mvn --batch-mode \
+  -P distribution \
+  -Dresume=false \
+  -Dtag=avocado-parent_2.11-${release} \
+  -DreleaseVersion=${release} \
+  -DdevelopmentVersion=${devel} \
+  -DbranchName=avocado_2.11-${release} \
+  release:clean \
+  release:prepare \
+  release:perform \
+  -Darguments="-DskipTests"
+
+if [ $? != 0 ]; then
+  echo "Releasing Scala 2.11 version failed."
+  exit 1
+fi
+
+if [ $branch = "master" ]; then
+  # if original branch was master, update versions on original branch
+  git checkout ${branch}
+  mvn versions:set -DnewVersion=${devel} \
+    -DgenerateBackupPoms=false
+  git commit -a -m "Modifying pom.xml files for new development after ${release} release."
+  git push origin ${branch}
+fi
